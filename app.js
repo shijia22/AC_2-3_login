e619a492a8d0bdb384f03e4b4a5a0c4acfe793a5
shichia22 <shichia0207@gmail.com>
2021-07-18 11:25:45 +0800

    feat:add log in page

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
+
 // index
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('login')
 })
 
 app.listen(3000, () => {
